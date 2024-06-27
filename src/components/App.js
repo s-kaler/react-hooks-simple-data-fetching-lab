7eef9541f5ad00eff7ec8162025e3d970aa20238
Sundeep <sundeep.kaler@gmail.com>
2024-06-26 20:33:26 -0400

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, {useEffect, useState} from "react";
+
+function App(){
+    const [dogPhoto, setDogPhoto] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            //console.log(data)
+            setDogPhoto(data.message);
+            //console.log(dogPhoto)
+            setIsLoaded(true);
+        });
+    }, []);
+
+  // if the data hasn't been loaded, show a loading indicator
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return <img alt="A Random Dog" src={dogPhoto}></img>;
+}
+
+export default App;
